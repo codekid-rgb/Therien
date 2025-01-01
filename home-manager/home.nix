834920abd82f986ed2283a36560c542f8a5836a8
Eugene Lord <eugene@lord.ac>
2024-12-31 19:21:22 -0700

    cleaned up

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -1,5 +1,3 @@
-# This is your home-manager configuration file
-# Use this to configure your home environment (it replaces ~/.config/nixpkgs/home.nix)
 {
   inputs,
   outputs,
@@ -8,39 +6,16 @@
   pkgs,
   ...
 }: {
-  # You can import other home-manager modules here
-  imports = [
-    # If you want to use modules your own flake exports (from modules/home-manager):
-    # outputs.homeManagerModules.example
-
-    # Or modules exported from other flakes (such as nix-colors):
-    # inputs.nix-colors.homeManagerModules.default
-
-    # You can also split up your configuration and import pieces of it here:
-    # ./nvim.nix
-  ];
+  imports = [];
 
   nixpkgs = {
-    # You can add overlays here
     overlays = [
-      # Add overlays your own flake exports (from overlays and pkgs dir):
       outputs.overlays.additions
       outputs.overlays.modifications
       outputs.overlays.unstable-packages
-
-      # You can also add overlays exported from other flakes:
-      # neovim-nightly-overlay.overlays.default
-
-      # Or define it inline, for example:
-      # (final: prev: {
-      #   hi = final.hello.overrideAttrs (oldAttrs: {
-      #     patches = [ ./change-hello-to-hi.patch ];
-      #   });
-      # })
     ];
-    # Configure your nixpkgs instance
+    
     config = {
-      # Disable if you don't want unfree packages
       allowUnfree = true;
     };
   };
@@ -50,94 +25,89 @@
     homeDirectory = "/home/eugene";
   };
 
-  # Add stuff for your user as you see fit:
-  programs.neovim.enable = true;
-   home.packages = with pkgs; [
-   element-desktop
-   discord
-   helix
-   killall
-   kitty
-   gimp
-   curl
-   dunst
-   p2pool
-  monero-cli
-  gthumb
-  monero-gui
-   wayland-protocols
-   nerdfonts
-   libnotify
-   davinci-resolve
-   swww
-  htop
-  btop
-   rofi-wayland
-  hyprlock
-  _1password-gui
-  vscode
-  darktable
-   glfw3
-   wl-color-picker
-   polkit
-   prismlauncher
-   openrgb-with-all-plugins
-   jdk21
-   firefox
-   patool
+  # System Packages
+  home.packages = with pkgs; [
+    _1password-gui
+    btop
+    curl
+    darktable
+    davinci-resolve
+    discord
+    dunst
+    element-desktop
+    firefox
+    gimp
+    glfw3
+    gthumb
+    helix
+    htop
+    hyprlock
+    jdk21
+    killall
+    kitty
+    libnotify
+    monero-cli
+    monero-gui
+    nerdfonts
+    openrgb-with-all-plugins
+    p2pool
+    patool
+    polkit
+    prismlauncher
+    rofi-wayland
+    swww
+    vscode
+    wayland-protocols
     wineWowPackages.waylandFull
-   ];
+    wl-color-picker
+  ];
 
-  # Import the Hyprland settings from ./hyprland/hyprland.nix
+  # Window Manager
   wayland.windowManager.hyprland = {
     enable = true;
     xwayland.enable = true;
     settings = import ./desktop/hyprland.nix;
+  };
 
-    };
-
-#waybar
-  programs.waybar = {
-    enable = true;
-    settings = import ./desktop/waybar.nix;
-
-    };
-    programs.vscode = {
-  enable = true;
-  package = pkgs.vscodium.fhs;
-};
-
-
-    programs.helix = {
+  # Programs
+  programs = {
+    git.enable = true;
+    helix = {
       settings = {
-  theme = "onedarker";
-  editor = {
-    line-number = "relative";
-    lsp.display-messages = true;
-  };
-  keys.normal = {
-    space.space = "file_picker";
-    space.w = ":w";
-    space.q = ":q";
-    esc = [ "collapse_selection" "keep_primary_selection" ];
+        theme = "onedarker";
+        editor = {
+          line-number = "relative";
+          lsp.display-messages = true;
+        };
+        keys.normal = {
+          space.space = "file_picker";
+          space.w = ":w";
+          space.q = ":q";
+          esc = ["collapse_selection" "keep_primary_selection"];
+        };
+      };
+    };
+    home-manager.enable = true;
+    neovim.enable = true;
+    vscode = {
+      enable = true;
+      package = pkgs.vscodium.fhs;
+    };
+    waybar = {
+      enable = true;
+      settings = import ./desktop/waybar.nix;
     };
   };
-};
 
+  # XDG Portal
+  xdg.portal = {
+    enable = true;
+    extraPortals = [pkgs.xdg-desktop-portal-gtk];
+  };
 
-
-   xdg.portal.enable = true;
-   xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
-
-
-
-  # Enable home-manager and git
-  programs.home-manager.enable = true;
-  programs.git.enable = true;
-
-  # Nicely reload system units when changing configs
+  # System Services
   systemd.user.startServices = "sd-switch";
 
-  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  # State Version
   home.stateVersion = "24.05";
 }
